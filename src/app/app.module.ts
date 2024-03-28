e934680ae112a24f4f267f53d9a91671185b9369
darioncr <darioncrmtz@gmail.com>
2024-03-27 23:20:29 -0600

    creamos servicios y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { StarshipListComponent } from './starship-list/starship-list.component';
+import { StarshipDetailsComponent } from './starship-details/starship-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    StarshipListComponent,
+    StarshipDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/starship-details/starship-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starship-details',
+  templateUrl: './starship-details.component.html',
+  styleUrls: ['./starship-details.component.css']
+})
+export class StarshipDetailsComponent {
+
+}
